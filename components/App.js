4a116aa3dee7af03e48fe3b9fdf73d6057bef12f
Madeline Kahn <mk-acct@outlook.com>
2021-04-03 22:09:00 -0400

    do not repeat escape

--- components/App.js
+++ components/App.js
@@ -61,6 +61,7 @@ class App extends React.Component {
 					break;
 				case "Escape":
 					if (!this.anyDialogsOpen()) {
+						if (e.repeat) {return;}
 						this.setState({submenu: "tabs"});
 					} else {
 						return;
